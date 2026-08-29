7e956440868241642e38e25b7c43eb064f4361a1
Howard Lewis Ship <hlship@gmail.com>
2026-08-29 13:31:51 -0700

    feat: add the dialog-ide manual

--- src/build.clj
+++ src/build.clj
@@ -2,15 +2,17 @@
   (:require [babashka.process :as p]))
 
 (defn local-build
-  [kind] 
-  (let [playbook (str "local-antora-playbook-" kind ".yml")]
-    (p/exec  "watchexec"
-             "--clear"
-             "--notify"
-             "--watch" (str "../" kind "/manual")
-             "--watch" playbook
-             "--watch" "ui-overrides"
-             "--watch" "lib"
-             "--debounce" "500ms"
-             "npx antora --stacktrace"
-             playbook)))
+  ([kind] 
+   (local-build kind "manual"))
+  ([kind doc-dir]  
+   (let [playbook (str "local-antora-playbook-" kind ".yml")]
+     (p/exec  "watchexec"
+              "--clear"
+              "--notify"
+              "--watch" (str "../" kind "/" doc-dir)
+              "--watch" playbook
+              "--watch" "ui-overrides"
+              "--watch" "lib"
+              "--debounce" "500ms"
+              "npx antora --stacktrace"
+              playbook))))
